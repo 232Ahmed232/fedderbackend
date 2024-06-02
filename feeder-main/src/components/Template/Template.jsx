88570780fcc75ff55e2206748f395afa0848340a
232Ahmed232 <143320776+232Ahmed232@users.noreply.github.com>
2024-06-02 18:54:09 +0500

    Update Template.jsx

--- feeder-main/src/components/Template/Template.jsx
+++ feeder-main/src/components/Template/Template.jsx
@@ -84,7 +84,7 @@ function Template() {
         console.log(user);
 
        try {
-         const response = await fetch("http://localhost:3000/api/template/item",{
+         const response = await fetch("https://fedder.vercel.app/api/template/item",{
              method:"POST",
              headers:{
                  'Content-Type':"application/json"
