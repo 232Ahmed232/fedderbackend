8b6f8f9c5551a687b55a917433c946b9ce560ef1
232Ahmed232 <143320776+232Ahmed232@users.noreply.github.com>
2024-06-02 18:53:29 +0500

    Update Signup.jsx

--- feeder-main/src/components/Signup/Signup.jsx
+++ feeder-main/src/components/Signup/Signup.jsx
@@ -40,7 +40,7 @@ function Signup() {
         e.preventDefault();
 
        try {
-         const response = await fetch("http://localhost:3000/api/auth/register",{
+         const response = await fetch("https://fedder.vercel.app/api/auth/register",{
              method:"POST",
              headers:{
                  'Content-Type':"application/json"
