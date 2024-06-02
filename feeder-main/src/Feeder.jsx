1767a671768581be409bc2fdce081f07a5e8f8bb
232Ahmed232 <143320776+232Ahmed232@users.noreply.github.com>
2024-06-02 19:12:08 +0500

    Update Feeder.jsx

--- feeder-main/src/Feeder.jsx
+++ feeder-main/src/Feeder.jsx
@@ -4,12 +4,12 @@ import 'react-toastify/dist/ReactToastify.css';
 import { Navigate, Outlet, Router } from 'react-router-dom'
 import { AuthContext } from './store/Store';
 import Header from './components/Header/Header';
-
+import axios from "axios"
 
 
 function Feeder() {
     const {isLoogedin} = useContext(AuthContext)
-
+    axios.defaults.withCredentials = true
 
     return (<>
         {(isLoogedin) ? <Header /> : ""}
