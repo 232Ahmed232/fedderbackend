16bcc33d870c2b0abd217c01333f984ee1d8995c
232Ahmed232 <143320776+232Ahmed232@users.noreply.github.com>
2024-06-03 09:41:43 +0500

    Delete server directory

--- server/router/contact_route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const contactForm = require("../controllers/contact_controller.js")
-
-router.route("/contact").post(contactForm)
-
-module.exports = router
